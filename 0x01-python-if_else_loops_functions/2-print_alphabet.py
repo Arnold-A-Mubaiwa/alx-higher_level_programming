c5b29a604527397d764e7c4c0beb9f8aa0379aa1
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-11-29 14:23:03 +0200

    adding end as a parameter

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
-import string
-print(string.ascii_lowercase)
+# #!/usr/bin/python3
+# import string
+# print(string.ascii_lowercase)
+for i in range(97, 123):
+    print(chr(i), end='')
