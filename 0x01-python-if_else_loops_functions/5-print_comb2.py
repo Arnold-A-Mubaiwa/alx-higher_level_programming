cbd180dd357c429b34fe826e7e9f74aafe047a6d
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-11-29 20:38:32 +0200

    updated print comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+# for i in range(100):
+#     print("{0:02}".format(i), end='')
+#     if i != 99:
+#         print(", ", end='')
+
 for i in range(100):
-    print("{0:02}".format(i), end='')
-    if i != 99:
-        print(", ", end='')
+    if i == 99:
+        print("{0}".format(i))
+    else:
+        print("{0:02}".format(i), end=', ')
