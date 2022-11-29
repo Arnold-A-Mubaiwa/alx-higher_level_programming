d0186fd1d181ce12ad059065f0f779893bdb2722
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-11-29 23:44:44 +0200

    removing characters

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,5 +5,5 @@ def remove_char_at(str, n):
     for index in range(len(str)):
         if index == n:
             continue
-        new_str +="{0}".format(str[index])
+        new_str += "{0}".format(str[index])
     return new_str
