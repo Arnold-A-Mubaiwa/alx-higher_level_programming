1ad2c2737332ccc5cd46190428ceaa8b60180541
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-11-29 23:06:29 +0200

    fizz buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    toprint = ''
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            toprint = "FizzBuzz"
+        elif num % 3 == 0:
+            toprint = "Fizz"
+        elif num % 5 == 0:
+            toprint = "Buzz"
+        else:
+            toprint = num 
+        print("{0}".format(toprint), end=" ")
