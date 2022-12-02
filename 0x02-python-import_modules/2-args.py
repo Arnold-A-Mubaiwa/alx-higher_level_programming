61d8ebf0322ed4881cf09db75c72f821490d3595
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-02 07:01:26 +0200

    fixing argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == '__main__':
+    from sys import argv
 
-num_of_args = len(argv)
+    num_of_args = len(argv)
 
-if num_of_args == 1:
-    print("{} arguments.".format(num_of_args-1))
-elif num_of_args == 2:
-    print("{} argument:".format(num_of_args-1))
-    print("{}: {}".format(num_of_args-1, argv[num_of_args-1]))
-else:
-    print("{} arguments:".format(num_of_args-1))
-    for i in range(1, num_of_args):
-        print("{}: {}".format(i, argv[i]))
+    if num_of_args == 1:
+        print("{} arguments.".format(num_of_args-1))
+    elif num_of_args == 2:
+        print("{} argument:".format(num_of_args-1))
+        print("{}: {}".format(num_of_args-1, argv[num_of_args-1]))
+    else:
+        print("{} arguments:".format(num_of_args-1))
+        for i in range(1, num_of_args):
+            print("{}: {}".format(i, argv[i]))
