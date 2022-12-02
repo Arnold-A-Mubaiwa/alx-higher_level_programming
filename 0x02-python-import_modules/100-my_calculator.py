86737c47cbf5014b8604e8376f307484b8494e53
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-02 09:23:09 +0200

    exit statement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,26 +4,17 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
     from sys import argv, exit
 
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
-    result = 0
-    if len(argv) == 4:
-        if operator == '+':
-            result = add(a, b)
-            print("{} {} {} = {}".format(a, operator, b, result))
-        elif operator == '-':
-            result = sub(a, b)
-            print("{} {} {} = {}".format(a, operator, b, result))
-        elif operator == '*':
-            result = mul(a, b)
-            print("{} {} {} = {}".format(a, operator, b, result))
-        elif operator == '/':
-            result = div(a, b)
-            print("{} {} {} = {}".format(a, operator, b, result))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    else:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    op = argv[2]
+    if op != '+' or op != '-' or op != '*' or op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    result = 0
+    a = int(argv[1])
+    b = int(argv[3])
+    result = add(a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
