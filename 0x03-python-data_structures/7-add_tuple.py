a088840829f0c67358db21bd031be8cea9117d8c
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-03 09:47:45 +0200

    bound to f* up

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    if len(tuple_a) < 2:
+        b = 0
+        if len(tuple_a) == 0:
+            a = 0
+    else:
+        a = tuple_a[0]
+        b = tuple_a[1]
+
+    if len(tuple_b) < 2:
+        d = 0
+        if len(tuple_b) == 0:
+            c = 0
+    else:
+        c = tuple_b[0]
+        d = tuple_b[1]
+
+    total_1 = a + c
+    total_2 = b + d
+    t = total_1, total_2
+    return t
