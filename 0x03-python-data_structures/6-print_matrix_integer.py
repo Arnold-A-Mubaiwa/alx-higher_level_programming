a4d12760a5fcef55e7686559833b7de2dff36852
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-03 08:00:17 +0200

    removing space from rnd of metrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
+            v = " "
+            if len(i)-1 == j:
+                v=''
             print("{}".format(i[j]), end=' ')
         print()
