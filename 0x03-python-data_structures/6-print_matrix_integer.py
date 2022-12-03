1442d546a1273924986824073be67140adf3c719
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-03 08:07:07 +0200

    adding whitespace

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             v = " "
-            if len(i)-1 == j:
-                v=''
-            print("{}".format(i[j]), end=v)
+            if len(i) - 1 == j:
+                v = ''
+            print("{}".format(i[j]), end = v)
         print()
