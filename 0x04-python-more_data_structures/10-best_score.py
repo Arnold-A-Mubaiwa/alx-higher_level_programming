159a693a5ad2468a25a36a600333579ffa35e5a2
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-08 06:25:48 +0200

    fixing function name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def best_socre(a_dictionary):
+def best_score(a_dictionary):
     best = None
     if len(a_dictionary) != 0 or a_dictionary != None:
         for  val in a_dictionary.values():
