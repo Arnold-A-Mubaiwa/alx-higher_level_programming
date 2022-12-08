00f149c8fae3158ab92490320ee3f61bb1e36b1d
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-08 23:16:19 +0200

    romain to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    rn_2 = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    rn_2 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     val = 0
     temp = 0
     if type(roman_string) == str or roman_string is not None:
@@ -21,5 +21,5 @@ def roman_to_int(roman_string):
                         val += (v - temp)
                         temp = v
         return val
-    elif roman_string== None:
+    elif roman_string is None:
         return 0
