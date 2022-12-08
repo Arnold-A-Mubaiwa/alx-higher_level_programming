afdc07dc9787ac211a34eea11dd1bd6dcd015117
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-08 06:39:00 +0200

    none type has no 0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     best = None
     best_mark = None
-    if len(a_dictionary) != 0 or a_dictionary != None:
+    if a_dictionary != None:
         for  key, val in a_dictionary.items():
             if best == None and best_mark == None:
                 best = key
@@ -13,5 +13,12 @@ def best_score(a_dictionary):
                     best = key
                     best_mark = val
     else:
-        return best_mark
-    return best_mark
+        return best
+    return best
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
