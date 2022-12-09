92e5ee911d6fb94c866ae28efa01f05375af8d96
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-09 09:04:17 +0200

    return 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,10 @@
 def weight_average(my_list=[]):
     total = 0
     y_total = 0
-    for item in my_list:
-        total += (item[0] * item[1])
-        y_total += item[1]
-    return total / y_total
+    if my_list:
+        for item in my_list:
+            total += (item[0] * item[1])
+            y_total += item[1]
+        return total / y_total
+    else:
+        return 0
