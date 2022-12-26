c9c12003979d95662718e7b2d4aee082269b172f
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-12-26 12:55:10 +0200

    4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,15 +26,3 @@ class Square:
                 raise ValueError('size must be >= 0')
         except (TypeError, ValueError):
                 raise TypeError("size must be an integer")
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
