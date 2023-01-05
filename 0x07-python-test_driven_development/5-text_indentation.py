d070c74a142df77eaec071cbbfbf5a1cbef25d9c
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2023-01-05 09:27:37 +0200

    change the string to list

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,8 +13,22 @@ def text_indentation(text):
         TypeError if the argument passed is not an integer
     """
     list_x = ['.', '?', ':']
-    for letter in text:
-        if letter in list_x:
+    str_l = list(text)
+    for i in range(len(str_l)):
+        if str_l[i] in list_x:
+            print(str_l[i])
             print()
+            str_l[i + 1] = ''
         else:
-            print(letter, end='')
+            print(str_l[i], end='')
+
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
